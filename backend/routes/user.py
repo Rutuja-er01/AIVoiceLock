1f021a4556fc67267c162d493ea0b2a351d4d756
Rutuja-er01 <rutujaahire.er01@gmail.com>
2026-07-30 01:23:32 +0530

    Completed authentication module with JWT and profile API

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
-from models.user_model import User,LoginUser
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from models.user_model import User, LoginUser
 from config.database import users_collection
+from utils.jwt import create_access_token, verify_access_token
+from utils.hash import hash_password, verify_password
 
 router = APIRouter()
+
+security = HTTPBearer()
 
 
 @router.post("/register")
@@ -10,11 +15,28 @@ def register_user(user: User):
 
     user_data = user.model_dump()
 
+    # Check if email already exists
+    existing_user = users_collection.find_one(
+        {
+            "email": user.email
+        }
+    )
+
+    if existing_user:
+        return {
+            "message": "Email already exists"
+        }
+
+    # Hash password before saving
+    user_data["password"] = hash_password(user.password)
+
     users_collection.insert_one(user_data)
 
     return {
         "message": "User registered successfully"
     }
+
+
 @router.post("/login")
 def login_user(user: LoginUser):
 
@@ -29,11 +51,40 @@ def login_user(user: LoginUser):
             "message": "User not found"
         }
 
-    if existing_user["password"] != user.password:
+    # Verify hashed password
+    if not verify_password(user.password, existing_user["password"]):
         return {
             "message": "Incorrect password"
         }
 
+    token = create_access_token(
+        {
+            "email": user.email
+        }
+    )
+
     return {
-        "message": "Login successful"
+        "message": "Login successful",
+        "access_token": token,
+        "token_type": "bearer"
+    }
+
+
+@router.get("/profile")
+def get_profile(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+
+    token = credentials.credentials
+
+    payload = verify_access_token(token)
+
+    if payload is None:
+        return {
+            "message": "Invalid Token"
+        }
+
+    return {
+        "message": "Access Granted",
+        "user": payload
     }
